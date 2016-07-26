473e7326ced857ea9495069ab207a963746663fc
Phillip Elie <dddddeveloper@gmail.com>
2016-07-26 00:07:13 -0400

    Updating names

--- volumeUp.sh
+++ volumeUp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/media/PiShare/Git/kiosk_TheWalkerFarm/dbuscontrol.sh volumeup #using dbus controller to send key strokes to omxplayer syntax: dbuscontrol.sh <variable>
+/media/PiShare/Git/kiosk_Transportation/dbuscontrol.sh volumeup #using dbus controller to send key strokes to omxplayer syntax: dbuscontrol.sh <variable>
 #echo -n "+" > /bin/omxfifo
 exit 0
