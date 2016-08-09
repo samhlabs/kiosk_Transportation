d05332628af604865f21b3e25f4e2d45dd57f589
Phillip Elie <dddddeveloper@gmail.com>
2016-08-09 11:57:05 -0400

    Adding two more StartOMX scripts

---
+++ startOMX2.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+sudo killall -TERM omxplayer.bin 2>/dev/null #Kill all previously running omxplayer instances
+
+sleep .1 #breath... give that damn processor a break already
+
+#cat /bin/omxfifo | omxplayer -o local --win '300 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4
+#pipe a read from omxfifo by omxplayer
+
+#omxplayer -o local --win '300 0 1600 900' /media/PAGE/LINESHAFT_1318x900.mp4 < cat /bin/omxfifo
+#i think this is the same as above, just exhausting options
+
+# sleep 1.5 #dev delay - just testing.
+omxplayer -o local --win '96 0 1920 1080' /media/PiShare/transportation.mov #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
