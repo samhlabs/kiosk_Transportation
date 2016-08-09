571d017e544fbf1057de64e634247a1222653973
Phillip Elie <dddddeveloper@gmail.com>
2016-08-09 12:09:20 -0400

    Updating video file names

--- startOMX2.sh
+++ startOMX2.sh
@@ -11,4 +11,4 @@ sleep .1 #breath... give that damn processor a break already
 #i think this is the same as above, just exhausting options
 
 # sleep 1.5 #dev delay - just testing.
-omxplayer -o local --win '96 0 1920 1080' /media/PiShare/transportation.mov #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
+omxplayer -o local --win '96 0 1920 1080' /media/PiShare/railroads_no_caption.mp4 #launch the video // TO DO: replace this with variables for video and window size, or make easily replacable
